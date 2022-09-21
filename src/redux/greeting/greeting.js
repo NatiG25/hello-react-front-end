ce460eca9ddca654619cc503ea107fe36b6b5a43
NatiG <nati.g5684@gmail.com>
2022-09-21 23:30:18 +0300

    Install react-redux

--- src/redux/greeting/greeting.js
+++ src/redux/greeting/greeting.js
@@ -1,13 +1,13 @@
 const GREETING = 'GREETING';
 
-export const get_greeting = (payload) => ({ type: GREETING, payload });
+export const getGreeting = (payload) => ({ type: GREETING, payload });
 
 const url = 'http://[::1]:3000/api/v1/messages.json';
 export const fetchGreetings = () => async (dispach) => {
   const res = await fetch(url);
   const resJSON = await res.json();
 
-  dispach(get_greeting(resJSON));
+  dispach(getGreeting(resJSON));
 };
 
 const greetingReducer = (state = [], action) => {
