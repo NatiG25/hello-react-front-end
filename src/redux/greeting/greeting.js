6948acbbfb6c8b8376f8abc9df19e3b120b47172
NatiG <nati.g5684@gmail.com>
2022-09-22 20:37:30 +0300

    Remove json from the url

--- src/redux/greeting/greeting.js
+++ src/redux/greeting/greeting.js
@@ -2,7 +2,7 @@ const GREETING = 'GREETING';
 
 export const getGreeting = (payload) => ({ type: GREETING, payload });
 
-const url = 'http://[::1]:3000/api/v1/messages.json';
+const url = 'http://[::1]:3000/api/v1/messages';
 export const fetchGreetings = () => async (dispach) => {
   const res = await fetch(url);
   const resJSON = await res.json();
